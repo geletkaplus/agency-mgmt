d20eea4bb7ddfd45c0f281defe5aa803d590e1b4
jgeletka <john@geletkaplus.com>
2025-06-02 13:09:09 -0500

    adding

--- agency_management/agency_management/settings.py
+++ agency_management/agency_management/settings.py
@@ -129,3 +129,8 @@ STATICFILES_DIRS = [
 ]
 
 STATIC_ROOT = BASE_DIR / "staticfiles"
+
+# Authentication settings
+LOGIN_URL = '/accounts/login/'
+LOGIN_REDIRECT_URL = '/'
+LOGOUT_REDIRECT_URL = '/accounts/login/'
